277329a0a68689fbbd2c38f69fc51c3880c303d2
DELL <kothetnaingtun.ict@gmaiil.com>
2023-08-14 11:11:13 +0630

    test model

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -265,3 +265,6 @@ class FabricRequesttoWH(models.Model):
     request_usr = models.CharField(max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __st__(self):
+        return self.po_style_no
